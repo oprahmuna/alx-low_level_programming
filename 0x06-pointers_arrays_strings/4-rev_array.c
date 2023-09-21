d6be3ba3f73e17c031c54f607e749feeb0675f6e
opara samuel <oparachima45@gmail.com>
2023-09-21 04:59:28 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,11 +10,11 @@
   */
 void reverse_array(int *a, int n)
 {
-        int k, r;
-        for (k = 0; k < n--; k++)
-        {
-        r = a[k];
-        a[k] = a[n];
-        a[n] = r;
-        }
+int k, r;
+for (k = 0; k < n--; k++)
+{
+r = a[k];
+a[k] = a[n];
+a[n] = r;
+}
 }
