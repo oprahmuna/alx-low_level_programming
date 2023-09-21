339546b9e564c4d5132bfeb51e829ff99c5fe332
opara samuel <oparachima45@gmail.com>
2023-09-21 04:53:21 -0700

    strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,11 +13,11 @@ int _strcmp(char *s1, char *s2)
 int k = 0;
 while (s1[k] != '\0' && s2[k] != '\0')
 {
-        if (s1[k] != s2[k])
-        {
-        return (s1[k] - s2[k]);
-        }
-        k++;
-        }
-        return (0);
+if (s1[k] != s2[k])
+{
+return (s1[k] - s2[k]);
+}
+k++;
+}
+return (0);
 }
