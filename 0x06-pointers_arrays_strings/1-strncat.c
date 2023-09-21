e1780b4a4df77ea0c7eb1634c7aaeb9237a046ed
opara samuel <oparachima45@gmail.com>
2023-09-21 04:49:13 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,19 +12,19 @@
   */
 char *_strncat(char *dest, char *src, int n)
 {
-        int k, r;
-        k = 0;
-        r = 0;
-        while (dest[k] != '\0')
-        {
-        k++;
-        }
-        while (r < n && src[r] != '\0')
-        {
-        dest[k] = src[r];
-        k++;
-        r++;
-        }
-        dest[k] = '\0';
-        return (dest);
+int k, r;
+k = 0;
+r = 0;
+while (dest[k] != '\0')
+{
+k++;
+}
+while (r < n && src[r] != '\0')
+{
+dest[k] = src[r];
+k++;
+r++;
+}
+dest[k] = '\0';
+return (dest);
 }
