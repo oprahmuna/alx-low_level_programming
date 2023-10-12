7a7826a5ee1be542a39f5fd7c92ec18bde2a3fcf
opara samuel <oparachima45@gmail.com>
2023-10-12 04:03:34 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+typedef struct print_d
+{
+	char *c;
+	void (*f_pr)(va_list a);
+} print_d;
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif /* Variadic_functions */
