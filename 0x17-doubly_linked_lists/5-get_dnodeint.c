7db33c013b74ce7fca76ad71a06bf54b2cd5134b
opara samuel <oparachima45@gmail.com>
2023-12-08 09:26:16 -0800

    multiple commits

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get nth node of a linked list
+ * @head: pointer to head of list
+ * @index: nth node check
+ * Return: value at nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+dlistint_t *current;
+unsigned int i;
+current = head;
+for (i = 0; i <= index; i++)
+{
+if (!current)
+{
+return (NULL);
+}
+if (index == 0)
+{
+return (current);
+}
+if (i > 0)
+{
+current = current->next;
+}
+}
+return (current);
+}
